a56d9606e011ab2f9340a90722d226a13da35736
sxclij <sxclij@gmail.com>
2024-07-06 01:00:04 +0000

    good morning world

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    char file_data[65536];
+    uint32_t file_size;
+    FILE* file_ptr;
+
+    file_ptr = fopen("data.txt", "r");
+    file_size = fread(file_data, sizeof(char), sizeof(file_data), file_ptr);
+    file_data[file_size] = '\0';
+
+    puts(file_data);
+}
