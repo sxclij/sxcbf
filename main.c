4b5b33841a554a181ba7869781729bb07d753aee
sxclij <sxclij@gmail.com>
2024-07-08 09:13:25 +0000

    仮想命令、事前処理、ジャンプテーブル(fix)

--- main.c
+++ main.c
@@ -10,67 +10,115 @@
 #define mem_capacity (1 << 16)
 #define file_capacity (1 << 16)
 
+enum inst {
+    inst_null,
+    inst_nop,
+    inst_ptr_inc,
+    inst_ptr_dec,
+    inst_val_inc,
+    inst_val_dec,
+    inst_out,
+    inst_in,
+    inst_while_start,
+    inst_while_end,
+    inst_finish,
+};
+
 struct node {
-    uint16_t jmp;
+    enum inst inst;
     char x;
+    uint16_t jmp;
 };
 
 void exec(struct node* mem) {
     struct node* ip = &mem[0];
     struct node* ap = &mem[file_capacity];
-    uint_fast8_t lc = 0;
-
-    struct node* opt_loop;
 
     while (1) {
-        switch (ip->x) {
-            case '>':
+        switch (ip->inst) {
+            case inst_ptr_inc:
                 ap++;
+                ip++;
                 break;
-            case '<':
+            case inst_ptr_dec:
                 ap--;
+                ip++;
                 break;
-            case '+':
+            case inst_val_inc:
                 ap->x++;
+                ip++;
                 break;
-            case '-':
+            case inst_val_dec:
                 ap->x--;
+                ip++;
                 break;
-            case '.':
+            case inst_out:
                 putchar(ap->x);
+                ip++;
                 break;
-            case ',':
+            case inst_in:
                 ap->x = getchar();
+                ip++;
                 break;
-            case '[':
-                lc = (ap->x == 0);
-                while (lc) {
+            case inst_while_start:
+                if (ap->x != 0) {
                     ip++;
-                    lc += (ip->x == '[');
-                    lc -= (ip->x == ']');
-                }
-                break;
-            case ']':
-                if (ip->jmp != 0) {
+                } else {
                     ip = &mem[ip->jmp];
-                    break;
                 }
-                opt_loop = ip;
-                lc = 1;
-                while (lc) {
-                    ip--;
-                    lc -= (ip->x == '[');
-                    lc += (ip->x == ']');
-                }
-                ip--;
-                opt_loop->jmp = ip - mem;
                 break;
-            case '\0':
+            case inst_while_end:
+                ip = &mem[ip->jmp];
+                break;
+            case inst_finish:
                 return;
-            default:
+            case inst_nop:
+                ip++;
                 break;
         }
-        ip++;
+    }
+}
+void optimize(struct node* mem) {
+    uint16_t i = 0;
+    uint16_t jmptable_stack[file_capacity];
+    uint16_t jmptable_stack_size = 0;
+    while (1) {
+        switch (mem[i].x) {
+            case '>':
+                mem[i].inst = inst_ptr_inc;
+                break;
+            case '<':
+                mem[i].inst = inst_ptr_dec;
+                break;
+            case '+':
+                mem[i].inst = inst_val_inc;
+                break;
+            case '-':
+                mem[i].inst = inst_val_dec;
+                break;
+            case '.':
+                mem[i].inst = inst_out;
+                break;
+            case ',':
+                mem[i].inst = inst_in;
+                break;
+            case '[':
+                mem[i].inst = inst_while_start;
+                jmptable_stack[jmptable_stack_size++] = i;
+                break;
+            case ']':
+                mem[i].inst = inst_while_end;
+                mem[jmptable_stack[jmptable_stack_size - 1]].jmp = i+1;
+                mem[i].jmp = jmptable_stack[--jmptable_stack_size];
+                break;
+            case '\0':
+                mem[i].inst = inst_finish;
+                return;
+            default:
+                mem[i].inst = inst_nop;
+                break;
+        }
+        i++;
     }
 }
 
@@ -83,6 +131,7 @@ int main() {
     for (uint32_t i = 0; buf[i] != '\0'; i++)
         mem[i].x = buf[i];
 
+    optimize(mem);
     exec(mem);
     printf("\n\n%fs\n", ((double)clock()) / CLOCKS_PER_SEC);
 }
