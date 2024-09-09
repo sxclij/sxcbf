79bde2f02d9278f3872343d86b7187ec31fd603e
sxclij <sxclij@gmail.com>
2024-09-09 03:02:38 +0000

    ok

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 int main() {
+    clock_t clock_start = clock();
+
     char file_data[65536];
     FILE* file_ptr = fopen("data.txt", "r");
-    uint32_t file_size = fread(file_data, sizeof(char), sizeof(file_data), file_ptr);
+    uint_fast32_t file_size = fread(file_data, sizeof(char), sizeof(file_data), file_ptr);
     file_data[file_size] = '\0';
     fclose(file_ptr);
 
-    uint_fast32_t bf_ap = 0;
-    uint_fast32_t bf_ip = 0;
-    uint_fast32_t bf_mem[(1 << 16)];
+    register uint_fast16_t bf_ap = 0;
+    register uint_fast16_t bf_ip = 0;
+    __attribute__((aligned(16))) uint_fast8_t bf_mem[(1 << 16)];
     memset(bf_mem, 0, sizeof(bf_mem));
     while (1) {
         if (file_data[bf_ip] == '>') {
@@ -24,7 +31,7 @@ int main() {
             bf_mem[bf_ap]--;
         } else if (file_data[bf_ip] == '[') {
             if (bf_mem[bf_ap] == 0) {
-                uint_fast32_t nest = 1;
+                uint_fast8_t nest = 1;
                 while (nest) {
                     bf_ip++;
                     if (file_data[bf_ip] == '[') {
@@ -36,7 +43,7 @@ int main() {
                 }
             }
         } else if (file_data[bf_ip] == ']') {
-            uint_fast32_t nest = 1;
+            uint_fast8_t nest = 1;
             while (nest) {
                 bf_ip--;
                 if (file_data[bf_ip] == '[') {
@@ -46,11 +53,15 @@ int main() {
                     nest++;
                 }
             }
+            bf_ip--;
         } else if (file_data[bf_ip] == '.') {
             putchar(bf_mem[bf_ap]);
         } else if (file_data[bf_ip] == ',') {
             bf_mem[bf_ap] = getchar();
+        } else if (file_data[bf_ip] == '\0') {
+            break;
         }
         bf_ip++;
     }
+    printf("\n%f\n", (double)(clock() - clock_start) / CLOCKS_PER_SEC);
 }
